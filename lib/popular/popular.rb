073199145c5ab76e1a2cd2e91d4b31efe68abbdd
thejchap <commonmodestudio@gmail.com>
2014-03-30 13:22:16 -0400

    Update docs

--- lib/popular/popular.rb
+++ lib/popular/popular.rb
@@ -15,7 +15,14 @@ module Popular
 
     # Adds a friend to an instance's friend's list
     # 
-    # @param [PopularModel] a popular_model that the instance is not already friends with
+    # @param [Object] new_friend a popular_model that the instance is not already friends with
+    #
+    # @example
+    #   user = User.create name: "Justin"
+    #   other_user = User.create name: "Jenny"
+    #   user.befriend other_user
+    #
+    #   user.friends_with? other_user #=> true
     def befriend new_friend
       friendships.create friend: new_friend
     end
@@ -23,14 +30,29 @@ module Popular
     # Adds a friend to an instance's friend's list
     # Similar to .befriend, but will raise an error if the operation is not successful
     # 
-    # @param [PopularModel] a popular_model that the instance is not already friends with
+    # @param [Object] new_friend a popular_model that the instance is not already friends with
+    #
+    # @example
+    #   user = User.create name: "Justin"
+    #   other_user = User.create name: "Jenny"
+    #   user.befriend! other_user
+    #   user.reload
+    #   user.friends_with? other_user # => true
     def befriend! new_friend
       friendships.create! friend: new_friend
     end
 
     # Removes a friend from an instance's friend's list
     # 
-    # @param [PopularModel] a popular_model in this instance's friends list
+    # @param [Object] friend a popular_model in this instance's friends list
+    #
+    # @example
+    #   user = User.create name: "Justin"
+    #   other_user = User.create name: "Jenny"
+    #   user.befriend other_user
+    #   user.unfriend other_user
+    #
+    #   user.friends_with? other_user # => false
     def unfriend friend
       friendships.where( friend: friend ).first.destroy
     end
@@ -38,8 +60,18 @@ module Popular
     # Helper method for finding whether or not the instance is friends with 
     # another given popular_model
     # 
-    # @param [PopularModel] a popular_model
+    # @param [Object] popular_model
     # @return [Boolean] if the instance has popular_model as a friend
+    #
+    # @example
+    #   user = User.create name: "Justin"
+    #   other_user = User.create name: "Jenny"
+    #
+    #   user.friends_with? other_user #=> false
+    #
+    #   user.befriend other_user
+    #
+    #   user.friends_with? other_user #=> true
     def friends_with? popular_model
       friendships.where( friend: popular_model ).any?
     end
