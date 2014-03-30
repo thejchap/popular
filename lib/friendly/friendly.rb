343c43679da9f0828aeac0d8e1d5f386476d44bd
thejchap <commonmodestudio@gmail.com>
2014-03-30 10:31:24 -0400

    Some docs

--- lib/friendly/friendly.rb
+++ lib/friendly/friendly.rb
@@ -13,10 +13,16 @@ module Friendly
 
     end
 
+    # Adds a friend to an instance's friend's list
+    # 
+    # @param [FriendlyModel] a friendly_model that the instance is not already friends with
     def befriend new_friend
       friendships.create friend: new_friend
     end
 
+    # Removes a friend from an instance's friend's list
+    # 
+    # @param [FriendlyModel] a friendly_model in this instance's friends list
     def unfriend friend
       friendships.where( friend: friend ).first.destroy
     end
